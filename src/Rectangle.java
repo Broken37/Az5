780211d369e98e25084799f894ca9dd811a86f65
Hamed Abdi <hamed.abdi@balad.ir>
2023-07-04 16:47:23 +0330

    feat(rectangle): Add getter and setter for rectangle width and height

--- src/Rectangle.java
+++ src/Rectangle.java
@@ -10,4 +10,19 @@ public class Rectangle {
     public int computeArea(){
         return width * height;
     }
+    public int getWidth(){
+        return width;
+    }
+
+    public void setWidth(int width){
+        this.width = width;
+    }
+
+    public int getHeight(){
+        return height;
+    }
+
+    public void setHeight(int height){
+        this.height = height;
+    }
 }
