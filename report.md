6dad7ad0c5f1b683701cd5c27899e542c5cf7bb3
Hamed Abdi <hamed.abdi@balad.ir>
2023-07-04 16:46:11 +0330

    feat(rectangle): Add class rectangle to calculate it's area

---
+++ report.md
@@ -0,0 +1,4 @@
+codes:
+
+first commit: در کامیت اول یه کلاس Rectangle تعریف کردم که دو پارامتر width و height را ورودی می‌گیرد و یه تابع محاسبه مساحت دارد
+second commit:
---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class PrototypePatternDemo {
+    public static void main(String[] args) {
+
+        System.out.println("Creating a Rectangle with width 3 and height 4.");
+        Rectangle r = Rectangle(3, 4)
+
+        System.out.println("Rectangle area is : " + r.computeArea());
+   }
+}
